3c22a8d3f133aacb55c179dad87a73f9039a9818
santorsm <matt.santorsola@gmail.com>
2021-04-07 21:20:10 -0700

    remove unused

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import React from 'react';
-import axios from 'axios';
+
+import CitySearch from './CitySearch';
 
 import './App.css';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       haveWeSearchedYet: false,
+       citySearchedFor: ''
+    };
+  }
+
+  handleShowSearch = () => {
+    this.setState({haveWeSearchedYet: false});
+  }
+
+  handleSearch = (citySearchedFor) => {
+    console.log('searched',citySearchedFor);
+    this.setState({
+      haveWeSearchedYet: true,
+      citySearchedFor: citySearchedFor
+    })
+  }
+  
   render() {
     return (
-      <h2>Hello!</h2>
+      <>
+        <h1>City Explorer</h1>
+        {this.state.haveWeSearchedYet ? <button onClick={this.handleShowSearch}>Search Again</button> : <CitySearch handleSearch={this.handleSearch} />}
+      </>
     )
   }
 }
