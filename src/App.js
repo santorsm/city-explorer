32ec95d58e947739f12a19ad3f09deeb1e56e882
santorsm <matt.santorsola@gmail.com>
2021-04-10 13:56:38 -0700

    updated error handling

--- src/App.js
+++ src/App.js
@@ -13,7 +13,8 @@ class App extends React.Component {
   
     this.state = {
        cityData: '',
-       searchResults: ''
+       searchResults: '',
+       error: ''
     };
   }
 
@@ -25,6 +26,7 @@ class App extends React.Component {
 
 
   handleSearch = async() => {
+    try{
       let locationiqResponse = await axios.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.searchResults}&format=json`);
 
       console.log(locationiqResponse);
@@ -32,6 +34,10 @@ class App extends React.Component {
         cityData: locationiqResponse.data[0]
       });
 
+    } catch (err) {
+    console.log(err);
+    this.setState({error: `${err.message}`});
+    }
   }  
   
   render() {
@@ -42,12 +48,13 @@ class App extends React.Component {
         <CitySearch handleSearch={this.handleSearch} updateCity={this.updateCity}/>
         
         {this.state.cityData.lat !== undefined ?
-        <Image src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.state.cityData.lat},${this.state.cityData.lon}&zoom=10&size=<200>x<200> fluid`}/> : ''}
+        <Image src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.state.cityData.lat},${this.state.cityData.lon}&zoom=12&size=<200>x<200> fluid`}/> : ''}
         {this.state.cityData.lat !== undefined ? <ListGroup variant="flush">
           <ListGroup.Item>City:   {this.state.cityData.display_name}</ListGroup.Item>
           <ListGroup.Item>Latitude: {this.state.cityData.lat}</ListGroup.Item>
           <ListGroup.Item>Longitude: {this.state.cityData.lon}</ListGroup.Item>
         </ListGroup> : ''}
+        {this.state.error ? <h3>{this.state.error}</h3> : ''}
       </>
     )
   }
