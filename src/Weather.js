fe4013aa72eefc100d93998558390e0df1d90377
santorsm <matt.santorsola@gmail.com>
2021-04-14 20:16:19 -0700

    updated card 1

---
+++ src/Weather.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class Weather extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Weather</h1>
+      </div>
+    )
+  }
+}
+
+export default Weather;
